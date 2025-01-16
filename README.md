1ddc423607866dfc4aa30c0559e0c42d2dd3e9b7
Matias <SaliskyMatias@gmail.com>
2025-01-16 10:59:10 -0300

    Challenge finalizado

---
+++ README.md
@@ -0,0 +1,6 @@
+# Challenge: Sortear Amigo
+
+## Funcionalidad
+- En el botón "Añadir", vamos agregando uno a uno los amigos que quieres sortear.
+
+- En el botón "Sortear Amigo", vamos a poder seleccionar aleatoriamente a alguno de los amigos que añadimos anteriormente.
--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+const agregarAmigo = () => {
+    let amigo = document.getElementById('amigo').value;
+    if(amigo === '') return alert('Por favor, inserte un nombre.');
+
+    amigos.push(amigo);
+    limpiarInput();
+    listarAmigos();
+}
+
+const limpiarInput = () => {
+    document.getElementById('amigo').value = '';
+}
+
+const listarAmigos = () => {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.innerHTML = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+const sortearAmigo = () => {
+    if(amigos.length === 0) return alert('No hay amigos para sortear');
+
+    let indiceAleatorio = Math.floor((Math.random() * amigos.length ));
+    const amigo = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo secreto sorteado es: ${amigo}`;
+    console.log(amigo); 
+}
